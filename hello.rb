c798ae6f824bd97711918beb41779612c60906d5
Balint Nemes <squarebe03@gmail.com>
2015-08-03 13:38:48 +0100

    separated HTML from ruby code

--- hello.rb
+++ hello.rb
@@ -1,11 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "<div style = 'border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  <div>"
-end
-
-get '/secret' do
-	'This is a secret message'
+  erb :index
 end
